ffa481586826dd7e9e9b127ba7e3beecca4923c1
Pushkin-kumar <pushkin.10021997@gmail.com>
2024-02-29 13:16:08 +0530

    styled the div by JS

---
+++ script.js
@@ -0,0 +1,8 @@
+let div = document.querySelector(".myDiv");
+div.style.backgroundColor = "Yellow";
+div.style.margin = "20px";
+div.style.padding = "10px";
+div.style.fontSize = "18px";
+div.style.fontWeight = "bold";
+div.style.height = "200px";
+div.style.width = "300px";
